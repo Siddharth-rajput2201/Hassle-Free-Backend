43beb9fd981e7d112d4e8be54503b5f1d9d62d22
Siddharth-rajput2201 <rajputsiddharth18@gmail.com>
2022-03-08 03:05:34 +0530

    Added Delete Account Verification Complete

--- Auth/emailhelper.py
+++ Auth/emailhelper.py
@@ -29,3 +29,24 @@ def sendEmailVerification(inputEmail,inputUsername):
                 return jsonify({"message":"ACCOUNT ALREADY VERIFIED"}),400 
         except Exception as error:
             return jsonify({"message":"error"}),400
+
+def sendDeleteAccountVerification(inputEmail,inputUsername):
+    from app import mycursor
+    with smtplib.SMTP_SSL('smtp.gmail.com',465) as smtp:
+        try:
+            mycursor.execute("select EMAIL_VERIFICATION from Hassle_Free_Register where USERNAME = '{USER_NAME}';".format(USER_NAME = inputUsername))
+            STATUS = mycursor.fetchone()
+            if(STATUS[0]==True):
+                smtp.login(str(EMAIL_ADDRESS),str(EMAIL_PASSWORD))
+                token = jwt.encode({"username":inputUsername,"exp":datetime.datetime.utcnow() + datetime.timedelta(minutes=5)},SECRET_JWT_KEY, algorithm="HS256")
+                print(token)
+                link = "http://192.168.0.104:5000/delete/deleteaccount?t="+token.decode('utf-8')
+                subject = 'ACCOUNT DELETION REQUEST INTIATION - HASSLE FREE'
+                body = "Hello {username},\nWe are sad to see you gooo . :( \n WE BELIEVE IN PRIVACY\n Click here to delete your account : {deletionlink}".format(username = str(inputUsername) , deletionlink=str(link))
+                msg = f'SUBJECT:{subject}\n\n{body}'
+                smtp.sendmail(EMAIL_ADDRESS,inputEmail,msg)
+            else:
+                return jsonify({"message":"ACCOUNT NOT VERIFIED"}),400 
+        except Exception as error:
+            return jsonify({"message":"error"}),400
+
--- Delete/delete.py
+++ Delete/delete.py
@@ -1,6 +1,10 @@
 from flask import request , jsonify , blueprints
 import jwt
 from Auth.authhelper import check_for_token_email
+import bcrypt
+from Auth.emailhelper import sendDeleteAccountVerification
+import psycopg2
+
 
 delete_blueprint = blueprints.Blueprint('delete_blueprint',__name__)
 
@@ -30,32 +34,70 @@ def deletepasswords():
 
 
 
-@delete_blueprint.route('/deleteaccount' ,methods =['DELETE'])
-@check_for_token_email
+@delete_blueprint.route('/deleteaccount' ,methods =['GET'])
 def deleteaccount():
-   from app import mycursor,mydb,SECRET_PASSWORD,psycopg2
+   from app import mycursor,mydb,SECRET_JWT_KEY,psycopg2
+   args = request.args
+   token = args.get('t')
+   if not token:
+      return jsonify({"message":"TOKEN MISSING"}),400
    try:
-      TOKEN = request.form["JWT_TOKEN"]
-      if not TOKEN:
-         return jsonify({'message':'Missing Token'}),400
-      data =  jwt.decode(TOKEN,SECRET_PASSWORD,algorithms="HS256")
-      print(data['user_id'])
-      print(data['username'])
-      return("OK")
-   except Exception as error:
+      tokendata = jwt.decode(token,SECRET_JWT_KEY,algorithms="HS256")
+      mycursor.execute("select EMAIL_VERIFICATION,USER_ID from Hassle_Free_Register where USERNAME = '{USER_NAME}';".format(USER_NAME = tokendata['username']))
+      DATA = mycursor.fetchone()
+      if(DATA[0]==True):
+         mycursor.execute("delete from hassle_free_register where username = '{USER_NAME}';".format(USER_NAME = tokendata['username']))
+         mycursor.execute("drop table {TABLENAME};".format(TABLENAME = tokendata['username'] + "_" + str(DATA[1])))
+         mydb.commit()
+         return jsonify({"message":"ACCOUNT DELETED SUCCESSFULLY"}),201
+      else:
+         return jsonify({"message":"EMAIL NOT VERIFIED"}),200
+   except psycopg2.Error as error:
       print(error)
-      return jsonify({"message":"error"}),404 
-   #    PASSWORD_ID = request.form["PASSWORD_ID"]
-   #    if not PASSWORD_ID:
-   #       return jsonify({'message':'Missing Password ID'}),400
-   #    mycursor.execute("delete from {TABLENAME} where PASSWORD_ID = '{PASSWORDID}';".format(TABLENAME = data['username'] + "_" + str(data["user_id"]),PASSWORDID=PASSWORD_ID))
-   #    mydb.commit()
-   #    # mycursor.execute("select * from {TABLENAME};".format(TABLENAME = data['username'] + "_" + str(data['user_id'])))
-   #    # fetchData = mycursor.fetchall()
-   #    return jsonify({"message":"DELETE SUCCESSFULLY"}),200
-   # except Exception as error:
-   #    print(error)
-   #    return jsonify({"message":"error"}),404   
-   # except psycopg2.Error as error:
-   #    print(error)
-   #    return jsonify({"message":"error"}),403 
+      return jsonify({"message":"error"}),403
+   except Exception as error:
+      return str(error)
+   except:
+      return jsonify({"message":"UNAUTHORIZED"}),403
+
+
+
+@delete_blueprint.route('/delaccemail' ,methods =['POST'])
+def sendDelAccEmail():
+    from app import mycursor
+    try:
+        name = request.form['USER_NAME']
+        password = request.form['USER_PASSWORD']
+        if(len(name)==0):
+            return jsonify({"message":"USERNAME CANNOT BE EMPTY"})
+        if(len(password)==0):
+            return jsonify({"message":"PASSWORD CANNOT BE EMPTY"})
+
+        mycursor.execute("select USER_ID from Hassle_Free_Register where USERNAME = '{USER_NAME}';".format(USER_NAME = name))
+        data = mycursor.fetchone()
+        
+        # generation of token 
+        if data:
+            mycursor.execute("select PASSWORD from Hassle_Free_Register where USERNAME = '{USER_NAME}';".format(USER_NAME = name))
+            hashed_pass = mycursor.fetchone()
+            if bcrypt.checkpw(password.encode('utf-8'),str(hashed_pass[0]).encode('utf-8')):
+                mycursor.execute("select USER_ID,EMAIL_ID,USERNAME from Hassle_Free_Register where username = %s",[name]) 
+                data = mycursor.fetchone()
+                sendDeleteAccountVerification(data[1],data[2])
+            else:
+                return jsonify({"message":"INVALID CREDENTIALS"}),200
+        else:
+            return jsonify({"message":"USER DOES NOT EXIST"}),401
+        return jsonify({"message" : "EMAIL SUCCESSFULLY SENT"}),201
+    except TypeError as error:
+        print(error)
+        return jsonify({"message":"error"}),400
+    except ValueError as error:
+        print(error)
+        return jsonify({"message":"error"}),403
+    except psycopg2.Error as error:
+        print(error)
+        return jsonify({"message":"error"}),403
+    except Exception as error:
+        print(error)
+        return jsonify({"message":"error"}),403
