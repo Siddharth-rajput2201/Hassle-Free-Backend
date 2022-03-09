6c44b949b9b422bb2bab067222147736800f2848
Siddharth-rajput2201 <rajputsiddharth18@gmail.com>
2022-03-10 02:47:47 +0530

    Email Check

--- Auth/e_mailhelp.py
+++ Auth/e_mailhelp.py
@@ -54,7 +54,7 @@ def test(inputEmail,inputUsername):
             smtp.login(str(EMAIL_ADDRESS),str(EMAIL_PASSWORD))
             token = jwt.encode({"username":inputUsername,"exp":datetime.datetime.utcnow() + datetime.timedelta(minutes=5)},SECRET_JWT_KEY, algorithm="HS256")
             domain = "https://hassle-free.herokuapp.com/delete/deleteaccount?t="
-            link = domain + token
+            link = domain + token.decode('UTF-8')
             subject = 'ACCOUNT DELETION REQUEST INTIATION - HASSLE FREE'
             body = "Hello {username},\nWe are sad to see you gooo . :( \n WE BELIEVE IN PRIVACY\n Click here to delete your account : {deletionlink}".format(username = str(inputUsername) , deletionlink=str(link))
             msg = f'SUBJECT:{subject}\n\n{body}'
